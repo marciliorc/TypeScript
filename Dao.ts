4fc3e55f7c4e07554dee67c0a110af69451d71c3
MARCILIOREIS\email <emaildomarcilio@gmail.com>
2018-05-08 22:26:24 -0300

    TypeScript e POO em JavaScript

---
+++ Dao.ts
@@ -0,0 +1,31 @@
+import { DaoInterface } from "./DaoInterface";
+
+export class Dao<T> implements DaoInterface<T>{
+    
+    nomeTabela:  string = ''
+
+    inserir(object: T): boolean {
+        console.log('lógica de insert')
+        return true
+    }
+
+    atualizar(object: T): boolean{
+        console.log('lógica de update')
+        return true
+    }
+
+    remover(id: number): T{
+        console.log('lógica de delete')
+        return Object()
+    }
+
+    selecionar(id: number): T{
+        console.log('lógica de select')
+        return Object()
+    }
+
+    selecionarTodos(): [any]{
+        console.log('lógica de getAll')
+        return Object()
+    }
+}
